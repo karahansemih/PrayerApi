a6a03595842ef2d98b2e2fdcf572cbf3d6edbd57
Semih Karahan <karahansemih@hotmail.com>
2023-03-27 23:25:03 +0300

    Get location updated. Need to set async

--- PrayerApiStoryboard/ViewController.swift
+++ PrayerApiStoryboard/ViewController.swift
@@ -6,18 +6,35 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
-    var zamanlar = [String]()
+class ViewController: UIViewController, CLLocationManagerDelegate {
     
+    private var locationManager:CLLocationManager!
+    var lat = String()
+    var lng = String()
+    var zamanlar = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-            getPrayer()
-
         
+        locationManager = CLLocationManager()
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
+        locationManager.delegate = self
+
+        getPrayer()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        DispatchQueue.main.async {
+            if let location = locations.last {
+                self.lat = "\(location.coordinate.latitude)"
+                self.lng = "\(location.coordinate.longitude)"
+                print(self.lat)
+                print(self.lng)
+            }
+        }
     }
 
     func getPrayer(){
@@ -25,49 +42,38 @@ class ViewController: UIViewController {
         let sem = "\(datez)"
         let sem2 = sem.components(separatedBy: " ")
         
-        let urlString = "https://namaz-vakti.vercel.app/api/timesFromCoordinates?lat=39.91987&lng=32.85427&date=\(sem2[0])&days=3&timezoneOffset=180"
+        let urlString = "https://namaz-vakti.vercel.app/api/timesFromCoordinates?lat=\(self.lat)&lng=\(self.lng)&date=\(sem2[0])&days=3&timezoneOffset=180"
         guard let url = URL(string: urlString) else { return }
-            
             URLSession.shared.dataTask(with: url) { data, response, err in
                 guard let data = data, err == nil else { return }
-                
                 do {
                     let jsonData = try JSONDecoder().decode(Datas.self, from: data)
                     let ulke = jsonData.place.country
-                    let sehir = jsonData.place.city
+                    let ilce = jsonData.place.city
+                    let il = jsonData.place.region
                     let zaman = jsonData.times.keys
                     for pip in zaman{
                         self.zamanlar.append(pip)
                     }
-                    //print(self.zamanlar[0])
                     let denemee = jsonData.times[self.zamanlar[0]]!
-                    print(denemee)
-                    /*
                     print(ulke)
-                    print(sehir)
-                    print(type(of: zaman))
-                    */
-
-                     
+                    print(il)
+                    print(ilce)
+                    print(denemee)
+                    
                 } catch let jsonErr {
                     print("failed to decode json:", jsonErr)
                 }
-                
-            }.resume() // don't forget
-        
-        
-
+            }.resume()
     }
-    
-    
 }
+
 
 struct Datas: Decodable {
     let place: Place
     let times: [String: [String]]
 }
 
-// MARK: - Place
 struct Place: Codable {
     let countryCode, country, region, city: String
     let latitude, longitude: Double
